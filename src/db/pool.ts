b46bfe53be13187fdb5e96fb9a5d0a8931f067cc
hungtsou <hungtsougg@gmail.com>
2026-03-10 20:33:29 -0600

    Revert "chore(config): migrate db pool to use central env module"
    This reverts commit 7146d419c1658a507697ba94ba9e57fef46487d6.

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,7 +1,6 @@
 import { Pool } from 'pg';
-import { env } from '../config/env';
 
 // Exported as a singleton. Import this in query files, never create new Pool instances.
 export const pool = new Pool({
-  connectionString: env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
